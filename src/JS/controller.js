05f3f04a2c2ee1400f2859459f9ab6daa6a8a5c5
EslamXeno <eslam.O.elkhabery@gmail.com>
2025-08-12 19:23:32 +0300

    almost finish navbar

--- src/JS/controller.js
+++ src/JS/controller.js
@@ -1,4 +1,6 @@
-document.querySelector("button").addEventListener("click", (e) => {
+document.querySelector(".btn-toggle-theme").addEventListener("click", (e) => {
   e.preventDefault();
   document.body.classList.toggle("dark-theme");
+  document.querySelector(".btn-toggle-theme").classList.toggle("left");
+  document.querySelector(".sun").classList.toggle("rotate");
 });
