2ab9ce6bb467140b81e3401e8ccf4aa7bdbbd79f
Eslam Omar <eslam.O.elkhabery@gmail.com>
2025-08-13 05:08:59 +0300

    finish navbar

--- src/JS/controller.js
+++ src/JS/controller.js
@@ -4,3 +4,21 @@ document.querySelector(".btn-toggle-theme").addEventListener("click", (e) => {
   document.querySelector(".btn-toggle-theme").classList.toggle("left");
   document.querySelector(".sun").classList.toggle("rotate");
 });
+
+const searchBar = document.querySelector(".input-search-bar");
+
+const mq = window.matchMedia("(max-width: 1200px)");
+
+const updatePlaceholder = function (e) {
+  if (mq.matches) {
+    searchBar.placeholder = "";
+  } else {
+    searchBar.placeholder = "Search Over 1,000,0000 Recipes";
+  }
+};
+
+// Run on page load
+updatePlaceholder(mq);
+
+// Run whenever the screen size matches/unmatches
+mq.addEventListener("change", updatePlaceholder);
