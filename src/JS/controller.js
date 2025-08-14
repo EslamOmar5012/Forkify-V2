a77143d60234212f10c7f4095cabb909c3940b0d
EslamXeno <eslam.O.elkhabery@gmail.com>
2025-08-14 08:44:36 +0300

    add resault and recipe sections

--- src/JS/controller.js
+++ src/JS/controller.js
@@ -3,6 +3,22 @@ document.querySelector(".btn-toggle-theme").addEventListener("click", (e) => {
   document.body.classList.toggle("dark-theme");
   document.querySelector(".btn-toggle-theme").classList.toggle("left");
   document.querySelector(".sun").classList.toggle("rotate");
+  // document.querySelector(".recipes-stored").insertAdjacentHTML(
+  //   "beforeend",
+  //   `<div class="bookmark">
+  //     <div class="image">
+  //       <img
+  //       src=${img}
+  //       alt="img"
+  //       />
+  //       </div>
+  //       <div class="text">
+  //         <p>Roasted Mushroom and Green Bean Farro Salad eslam omar</p>
+  //       <span>Closet Cooking</span>
+  //     </div>
+  //     <a href = ""></a>
+  //   </div>`
+  // );
 });
 
 const searchBar = document.querySelector(".input-search-bar");
